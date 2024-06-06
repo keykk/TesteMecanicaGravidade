13c5cf88e086dc45a6c8183375fc484d2ac7b543
keykk <keykkashi@gmail.com>
2024-06-06 02:30:40 -0300

    first commit

---
+++ CharacterBody3D.gd
@@ -0,0 +1,217 @@
+extends CharacterBody3D
+var input_map = {
+	"move_left": [KEY_LEFT, KEY_A],
+	"move_right": [KEY_RIGHT, KEY_D],
+	"move_forward": [KEY_UP, KEY_W],
+	"move_back": [KEY_DOWN, KEY_S],
+	"jump": [KEY_SPACE],
+	"zoom_in": [MOUSE_BUTTON_WHEEL_UP],
+	"zoom_out": [MOUSE_BUTTON_WHEEL_DOWN],
+	"camera_left":[KEY_J],
+	"camera_right":[KEY_L],
+	"camera_up":[KEY_I],
+	"camera_down":[KEY_K],
+	"attack_1":[MOUSE_BUTTON_LEFT]
+}
+@export var camera_view: Node3D
+@export_subgroup("Properties")
+@export var movement_speed: int = 250
+@export var jump_strength: int = 10
+@onready var planet_1 = $"../planet1"
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+@onready var node_3d = $"../Node3D"
+@export var planet_path : NodePath
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: Vector3
+var gravity_strength: float = 7
+var gravity_velocity: Vector3 = Vector3.ZERO
+
+#jump
+var jump_single: bool = true
+var jump_double: bool = true
+var jump_deceleration: float = 10.0
+var is_jumping: bool = false
+
+#direction
+var input_direction: Vector2 = Vector2.ZERO
+var forward_dir: Vector3
+var right_dir: Vector3
+var move_dir: Vector3 = Vector3.ZERO
+
+
+#orientation
+var up_dir: Vector3
+var current_forward: Vector3
+var orientation: Basis
+
+var projected_position: Vector3 = Vector3.ZERO
+var planet_direction: Vector3 = Vector3.ZERO
+
+func _ready():
+	load_input_map()
+	
+func _physics_process(delta):
+	# Add the gravity
+	handle_controls(delta)
+	apply_gravity(delta)
+	handle_orientation(delta)
+	
+	velocity += gravity_velocity
+	up_direction = -gravity.normalized()
+	
+
+	move_and_slide()
+	
+	projected_position = get_projected_position_on_planet()
+	planet_direction = gravity.normalized()
+	
+	
+func apply_gravity(delta) -> void:
+	var sphere_node = get_node_or_null(planet_path)
+	if sphere_node:
+		var dir_to_planet_center: Vector3 = sphere_node.global_transform.origin - global_transform.origin
+		gravity = dir_to_planet_center.normalized() * gravity_strength
+		gravity_velocity += gravity * delta
+		#
+	# Taking jump into account
+	if is_jumping and gravity_velocity.dot(-gravity.normalized()) > 0:
+		gravity_velocity += gravity.normalized() * jump_deceleration * delta  # Upward deceleration to get a smooth jump
+	else:
+		is_jumping = false
+		gravity_velocity += gravity * delta
+#
+	if is_on_floor():
+		gravity_velocity = gravity_velocity.lerp(Vector3.ZERO, 10 * delta)
+		jump_single = true
+		jump_double = true
+		
+func handle_controls(delta) -> void:
+	input_direction = Vector2.ZERO
+	input_direction.y = Input.get_axis("move_right", "move_left")
+	input_direction.x = Input.get_axis("move_back", "move_forward")
+	
+	input_direction = input_direction.normalized()
+
+	right_dir = -transform.basis.x
+	forward_dir = -transform.basis.z
+
+	move_dir = (forward_dir * input_direction.x + right_dir * input_direction.y).normalized()
+	#move_dir = move_dir.rotated(Vector3.UP, camera_view.rotation.y).normalized()
+	velocity = move_dir * movement_speed * delta
+	
+	if Input.is_action_just_pressed("jump"):
+		if jump_single:
+			jump_single = false
+			jump_action()
+		elif jump_double:
+			jump_double = false
+			jump_action()
+	
+
+func handle_orientation(delta) -> void:
+	#update_player_rotation()
+	# Step 1: Align the player with the planet
+	up_dir = -gravity.normalized()
+	current_forward = global_transform.basis.z # This might cause the resetting while leaving keys
+
+	# Ensure move_dir is orthogonal to up_dir
+	move_dir = (move_dir - up_dir * move_dir.dot(up_dir)).normalized()
+
+	# Calculate a forward direction. If the current forward direction 
+	# is almost parallel to up_dir, choose a different reference axis for stability.
+	if abs(current_forward.dot(up_dir)) > 0.98:  # nearly parallel
+		current_forward = global_transform.basis.x  # Use the side vector as a reference
+
+	# Make sure the new forward direction is orthogonal to up_dir
+	forward_dir = (current_forward - up_dir * current_forward.dot(up_dir)).normalized()
+
+	# Calculate the right direction based on the new forward and up directions
+	right_dir = up_dir.cross(forward_dir).normalized()
+
+	# Build the orientation matrix
+	orientation = Basis(right_dir, up_dir, forward_dir)
+	global_transform.basis = orientation
+	
+func jump() -> void:
+	
+	jump_action()
+	
+	scale = Vector3(0.5, 1.5, 0.5)
+	
+	jump_single = false;
+	jump_double = true;
+
+func jump_action() -> void:
+	if is_on_floor() or jump_double:
+		scale = Vector3(0.5, 1.5, 0.5)
+		gravity_velocity = -gravity.normalized() * jump_strength
+		is_jumping = true
+	
+func load_input_map():
+	#vincular mapa
+	for action in input_map.keys():
+		var keys = input_map[action]
+		for key in keys:
+			var tecla = InputEventKey.new()
+			tecla.keycode = key
+			
+			var input_mouse_button = InputEventMouseButton.new()
+			
+			#adicionaa ação
+			if not InputMap.has_action(action):
+				InputMap.add_action(action)
+				
+			if key == 1 || key == 2:
+				input_mouse_button.pressed = true
+				input_mouse_button.button_index = key
+				InputMap.action_add_event(action, input_mouse_button)
+				
+				pass
+			else:
+				InputMap.action_add_event(action, tecla)	
+	#print(str(input_map))
+
+func clear_input_map():
+	# desvincular antigo
+	for action in input_map.keys():
+		var keys = input_map[action]
+		for key in keys:
+			var tecla = InputEventKey.new()
+			tecla.keycode = key
+			
+			var input_mouse_button = InputEventMouseButton.new()
+			
+			if key == 1 || key == 2:
+				input_mouse_button.pressed = true
+				input_mouse_button.button_index = key
+				InputMap.action_erase_event(action, input_mouse_button)	
+				
+				pass
+			else:
+				InputMap.action_erase_event(action, tecla)	
+
+func get_projected_position_on_planet() -> Vector3:
+	var sphere_node: Node = get_node_or_null(planet_path)
+	if sphere_node:
+		var normalized_dir = planet_direction.normalized()
+		var planet_radius = sphere_node.scale.x * 0.5  # Assuming the planet is a perfect sphere with diameter = scale.x
+		return sphere_node.global_transform.origin + normalized_dir * planet_radius
+	return Vector3.ZERO
+	
+func update_player_rotation():
+	# Pegar a base atual do jogador
+	var player_transform = global_transform
+	
+	# Criar uma nova base para rotação horizontal
+	var rotation_transform = Transform3D()
+	rotation_transform.basis = Basis()
+	rotation_transform.basis = rotation_transform.basis.rotated(Vector3(0, 1, 0), deg_to_rad(camera_view.camera_rotation.y))
+	
+	# Aplicar a rotação horizontal ao jogador mantendo a orientação vertical
+	player_transform.basis = rotation_transform.basis * player_transform.basis
+	
+	# Atualizar a transformação global do jogador
+	global_transform = player_transform
---
+++ camera_pivot.gd
@@ -0,0 +1,175 @@
+extends Node3D
+@export_group("Properties")
+
+@export var target: CharacterBody3D
+
+@export_group("Zoom")
+@export var zoom_minimum = 16
+@export var zoom_maximum = 4
+@export var zoom_speed = 10
+
+@export_group("Rotation")
+@export var rotation_speed = 120
+@export var min_rotation_x = -80
+@export var max_rotation_x = -10
+
+@export_group("Mouse")
+@export var mouse_sensitivity : float = 0.1
+
+var camera_rotation:Vector3
+var target_rotation:Vector3
+var zoom = 10
+var is_right_mouse_button_pressed = false
+var last_mouse_position = Vector2()
+var input := Vector3.ZERO
+var input_target := Vector3.ZERO
+@onready var planet = $"../planet1"
+
+@onready var camera = $camera
+
+var smoothing: float = 10
+var last_position: float = 0.0
+var player
+const VERTICAL_LIMIT = 60 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	camera_rotation = rotation_degrees # Initial rotation
+	player = target
+	pass
+
+
+func _physics_process(delta):
+	self.position = self.position.lerp(target.position, delta * 4)
+	
+	camera.position = camera.position.lerp(Vector3(0, 0, zoom), 8 * delta)
+	handle_input(delta)
+	global_transform.basis = target.orientation
+	update_camera(delta)
+	pass
+
+# Handle input
+
+func handle_input(delta):
+	# Rotation
+	input.y = Input.get_axis("camera_left", "camera_right")
+	input.x = Input.get_axis("camera_up", "camera_down")
+	
+	#camera_rotation += input.limit_length(1.0) * rotation_speed * delta
+	#camera_rotation.x = clamp(camera_rotation.x, min_rotation_x, max_rotation_x)
+	# Zooming
+	
+	zoom += Input.get_axis("zoom_in", "zoom_out") * zoom_speed * delta
+	zoom = clamp(zoom, zoom_maximum, zoom_minimum)
+	
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		
+		if event.pressed:		
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				zoom -= lerpf(zoom, zoom_speed, 0.1)
+			elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				zoom += lerpf(zoom, zoom_speed, 0.1) 
+			zoom = clamp(zoom,zoom_maximum, zoom_minimum)
+	
+	if event is InputEventMouseMotion:
+		var mouse_movement = event.relative
+		#
+		#var input := Vector3.ZERO
+		#input.x = (mouse_movement.y * mouse_sensitivity) * -1
+		#input.y = (mouse_movement.x * mouse_sensitivity) * -1
+	#
+		#camera_rotation += input.limit_length(1.0) * 4
+		
+		#
+		#print(mouse_movement)
+		#var current_mouse_position = event.position
+		#var delta = current_mouse_position - last_mouse_position
+		#last_mouse_position = current_mouse_position
+		rotate_camera(mouse_movement)
+
+func rotate_camera(delta):
+	input.x = (delta.y * mouse_sensitivity) * -1
+	input_target.y = (delta.x * mouse_sensitivity) * -1
+	
+	camera_rotation += input.limit_length(1.0) * 4
+	target_rotation += input_target.limit_length(1.0) * 1
+	target_rotation.y = clamp(target_rotation.y, -10, 10)
+	camera_rotation.x = clamp(camera_rotation.x, -VERTICAL_LIMIT, VERTICAL_LIMIT)
+	update_player_rotation()
+
+
+func update_player_rotation():
+	# Pegar a base atual do jogador
+	
+	var player_transform = player.global_transform
+	
+	# Criar uma nova base para rotação horizontal
+	var rotation_transform = Transform3D()
+	rotation_transform.basis = Basis()
+	rotation_transform.basis = rotation_transform.basis.rotated(Vector3(0, 1, 0), deg_to_rad(target_rotation.y))
+	
+	# Aplicar a rotação horizontal ao jogador mantendo a orientação vertical
+	player_transform.basis = rotation_transform.basis * player_transform.basis
+	
+	# Atualizar a transformação global do jogador
+	player.global_transform = player_transform
+	apply_mouse_rotation_target()
+	
+		
+#func update_player_rotation():
+	## Calcular a nova direção do jogador após a rotação horizontal da câmera
+	#var new_player_forward = player.global_transform.basis.z.rotated(Vector3(0, 1, 0), deg_to_rad(camera_rotation.y))
+	#
+	## Calcular a rotação necessária para a nova direção do jogador
+	#var look_rotation = Basis(new_player_forward, Vector3(0, 1, 0)).get_rotation_quat()
+	#
+	## Aplicar a nova rotação ao jogador
+	#player.global_transform.basis = Basis(look_rotation)
+	
+func update_camera(delta):
+	## Posição do jogador e do planeta
+	#var player_position = player.global_transform.origin
+	#var planet_position = planet.global_transform.origin
+	#
+	## Vetor da direção do planeta para o jogador
+	#var to_player = (player_position - planet_position).normalized()
+	#
+	## Calcular a nova orientação da câmera
+	#var target_orientation = Transform3D()
+	#target_orientation.origin = global_transform.origin  # Manter a posição da câmera
+	#target_orientation.basis = Basis()  # Resetar a base
+	#
+	## Definir o eixo "up" da câmera como o vetor de direção do planeta para o jogador
+	#target_orientation.basis.y = to_player
+	#
+	## Definir o eixo "forward" da câmera como a direção que o jogador está olhando
+	#target_orientation.basis.z = (player.global_transform.basis.z).normalized()
+	#
+	## Corrigir o eixo "x" com um produto vetorial para manter a ortogonalidade
+	#target_orientation.basis.x = target_orientation.basis.y.cross(target_orientation.basis.z).normalized()
+	#
+	## Recalcular o eixo "z" para garantir a ortogonalidade
+	#target_orientation.basis.z = target_orientation.basis.x.cross(target_orientation.basis.y).normalized()
+	#
+	## Interpolar suavemente a rotação da câmera
+	#global_transform.basis = global_transform.basis.slerp(target_orientation.basis, 0.18)
+	
+	apply_mouse_rotation()
+	
+func apply_mouse_rotation():
+	var rotation_transform = Transform3D()
+	rotation_transform.basis = Basis()
+	rotation_transform.basis = rotation_transform.basis.rotated(Vector3(1, 0, 0), deg_to_rad(camera_rotation.x))
+	rotation_transform.basis = rotation_transform.basis.rotated(Vector3(0, 1, 0), deg_to_rad(camera_rotation.y))
+	
+	# Aplicar a rotação adicional à câmera
+	global_transform.basis = global_transform.basis * rotation_transform.basis
+	
+func apply_mouse_rotation_target():
+	var rotation_transform = Transform3D()
+	rotation_transform.basis = Basis()
+	#rotation_transform.basis = rotation_transform.basis.rotated(Vector3(1, 0, 0), deg_to_rad(target_rotation.x))
+	rotation_transform.basis = rotation_transform.basis.rotated(Vector3(0, 1, 0), deg_to_rad(target_rotation.y))
+	
+	# Aplicar a rotação adicional à câmera
+	target.global_transform.basis = global_transform.basis * rotation_transform.basis	
